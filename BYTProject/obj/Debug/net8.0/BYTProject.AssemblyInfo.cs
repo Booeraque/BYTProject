34bf169b660f25e471a9b9fbd11ac96ac918a6e1
s26904 <s26904@pjwstk.edu.pl>
2024-11-10 18:32:10 +0100

    AssemblyChanges

--- BYTProject/obj/Debug/net8.0/BYTProject.AssemblyInfo.cs
+++ BYTProject/obj/Debug/net8.0/BYTProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BYTProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40ee570c1a2b2ac1d7000c7da23b53ed17b2595")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fa6103f0abd21cbfd8a70109a3d6293ef710dae")]
 [assembly: System.Reflection.AssemblyProductAttribute("BYTProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BYTProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
